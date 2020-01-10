f937db786f69f01c9fd51c9f9076c8c36a525cfc
jg116907 <jg116907@naver.com>
2020-01-10 18:25:06 +0900

    ADD axios and state

--- src/components/CommentList/CommentList.js
+++ src/components/CommentList/CommentList.js
@@ -2,12 +2,21 @@ import React from 'react';
 import './CommentList.css';
 import { Comment } from '../';
 
-const CommentList = () => {
+const CommentList = ({comments}) => {
+  const commentList = comments.map(
+    (comment, index)=>(
+      <Comment
+        name={comment.email.split('@')[0]}
+        body={comment.body}
+        key={index}
+      />
+    )
+  );
+  
+  
   return (
     <ul className="CommentList">
-      <Comment/>
-      <Comment/>
-      <Comment/>
+      {commentList}
     </ul>
   );
 };
--- src/containers/PostContainers/PostContainer.js
+++ src/containers/PostContainers/PostContainer.js
@@ -1,12 +1,67 @@
 import React, { Component } from 'react';
 import { PostWrapper, Navigate, Post } from '../../components';
+import * as service from '../../services/post';
 
 class PostContainer extends Component{
+  constructor(props) {
+    super();
+    // initializes component state
+    this.state = {
+      postId: 1,
+      fetching: false,
+      post: {
+        title: null,
+        body: null
+      },
+      comments: []
+    };
+  }
+  fetchPostInfo = async (postId) => {
+    // 비효율적
+    // const post = await service.getPost(postId);
+    // console.log(post);
+    // const comments = await service.getComments(postId);
+    // console.log(comments);
+    this.setState({
+      fetching: true
+    });
+    const info = await Promise.all([
+      service.getPost(postId),
+      service.getComments(postId)
+    ]);
+    const { title, body } = info[0].data;
+
+    const comments = info[1].data;
+
+    this.setState({
+      postId,
+      post: {
+        title,
+        body
+      },
+      comments,
+      fetching: false
+    });
+    // console.log(info)
+  }
+
+  componentDidMount () {
+    this.fetchPostInfo(1);
+  }
+  
   render(){
+    const {postId, fetching, post, comments} = this.state;
     return(
       <PostWrapper>
-        <Navigate/>
-        <Post/>
+        <Navigate
+          postId={postId}
+          disabled={fetching}
+        />
+        <Post
+          title={post.body}
+          body={post.body}
+          comments={comments}
+        />
       </PostWrapper>
     );
   }
